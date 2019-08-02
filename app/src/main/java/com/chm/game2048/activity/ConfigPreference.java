057bbe9125d102f38e55cae9849f24e12f804669
chmmc6 <chmmc6@126.com>
2019-08-02 17:20:46 +0800

    Initial commit

---
+++ app/src/main/java/com/chm/game2048/activity/ConfigPreference.java
@@ -0,0 +1,130 @@
+package com.chm.game2048.activity;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.chm.game2048.R;
+import com.chm.game2048.config.Config;
+
+/**
+ * @date: 2018/6/18  18:43
+ */
+public class ConfigPreference extends Activity implements OnClickListener {
+
+    private Button mBtnGameLines;
+
+    private Button mBtnGoal;
+
+    private Button mBtnBack;
+
+    private Button mBtnDone;
+
+    private String[] mGameLinesList;
+
+    private String[] mGameGoalList;
+
+    private AlertDialog.Builder mBuilder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.config_preference);
+        initView();
+    }
+
+    private void initView() {
+        mBtnGameLines = (Button) findViewById(R.id.btn_gamelines);
+        mBtnGoal = (Button) findViewById(R.id.btn_goal);
+        mBtnBack = (Button) findViewById(R.id.btn_back);
+        mBtnDone = (Button) findViewById(R.id.btn_done);
+        mBtnGameLines.setText("" + Config.mSp.getInt(Config.KEY_GAME_LINES, 4));
+        mBtnGoal.setText("" + Config.mSp.getInt(Config.KEY_GAME_GOAL, 2048));
+        mBtnGameLines.setOnClickListener(this);
+        mBtnGoal.setOnClickListener(this);
+        mBtnBack.setOnClickListener(this);
+        mBtnDone.setOnClickListener(this);
+        mGameLinesList = new String[]{"4", "5", "6"};
+        mGameGoalList = new String[]{"1024", "2048", "4096"};
+    }
+
+    private void saveConfig() {//修改配置方法
+        Editor editor = Config.mSp.edit();
+        editor.putInt(Config.KEY_GAME_LINES,
+                Integer.parseInt(mBtnGameLines.getText().toString()));
+        editor.putInt(Config.KEY_GAME_GOAL,
+                Integer.parseInt(mBtnGoal.getText().toString()));
+        editor.commit();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_gamelines:
+                mBuilder = new AlertDialog.Builder(this);
+                mBuilder.setTitle("修改格子数");
+                mBuilder.setItems(mGameLinesList,
+                        new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                mBtnGameLines.setText(mGameLinesList[which]);
+                            }
+                        });
+                mBuilder.create().show();
+                break;
+            case R.id.btn_goal:
+                mBuilder = new AlertDialog.Builder(this);
+                mBuilder.setTitle("修改游戏目标");
+                mBuilder.setItems(mGameGoalList,
+                        new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                mBtnGoal.setText(mGameGoalList[which]);
+                            }
+                        });
+                mBuilder.create().show();
+                break;
+            case R.id.btn_back:
+                this.finish();
+                startActivity(new Intent(ConfigPreference.this,Game.class));
+                overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+                break;
+            case R.id.btn_done:
+                saveConfig();
+                setResult(RESULT_OK);
+                this.finish();
+                startActivity(new Intent(ConfigPreference.this,Game.class));
+                overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void lxzz(View v){
+
+        Toast.makeText(ConfigPreference.this,"16计算机2班陈泓谋",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // TODO Auto-generated method stub
+        if(keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount()==0)
+        {
+            this.finish();
+            startActivity(new Intent(ConfigPreference.this,Game.class));
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);//改变Activity跳转动画效果
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
---
+++ app/src/main/java/com/chm/game2048/activity/SplashActivity.java
@@ -0,0 +1,130 @@
+package com.chm.game2048.activity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.chm.game2048.R;
+
+//闪屏页
+public class SplashActivity extends AppCompatActivity implements Animation.AnimationListener{
+
+    Button bt1;
+    int i=5;
+    int j=0;
+    Handler he = new Handler();
+    Handler he2 = new Handler();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //控制状态栏消失
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash2);
+
+        //动态动画
+        ImageView splashImage = (ImageView) findViewById(R.id.imgLogo);
+        Animation animation = new ScaleAnimation(1.0f, 1.2f, 1.0f, 1.2f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f); // 将图片放大1.2倍，从中心开始缩放
+        animation.setDuration(5000); // 动画持续时间
+        animation.setFillAfter(true); // 动画结束后停留在结束的位置
+        animation.setAnimationListener(this);
+        splashImage.startAnimation(animation);
+
+
+//        隐藏状态栏方法二
+//        RelativeLayout layout = (RelativeLayout) findViewById(R.id.lay_shanping);
+//        layout.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+
+        bt1=(Button)findViewById(R.id.bt1);
+        new Thread(new MyCountDownTimer()).start();    //开始执行10s方法
+
+
+        //点击按钮就关闭
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                j=1;
+                startActivity(new Intent(SplashActivity.this,Game.class));
+                finish();//关闭activity页面
+//                System.exit(0);//彻底关闭进程
+            }
+        });
+
+
+
+        //广告十秒自动关闭
+        he.postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                if(j==0){
+                    startActivity(new Intent(SplashActivity.this,Game.class));
+                    finish();
+                }
+            }
+
+        }, 5000);
+    }
+
+    //倒计时10s关闭广告内部类
+    class MyCountDownTimer implements Runnable {
+
+        @Override
+        public void run() {
+
+            //倒计时开始，循环
+            while (i > -1) {
+                he2.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        bt1.setText(i + "跳过");
+                    }
+                });
+                try {
+                    Thread.sleep(1000); //强制线程休眠1秒，就是设置倒计时的间隔时间为1秒。
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                i--;
+            }
+        }
+    }
+
+
+    //广告页点击返回键直接退出app（onKeyDown）
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN){
+            j=1;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+    //动画方法
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+//        startActivity(new Intent(SplashActivity.this,MainActivity.class));
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
+
+}
